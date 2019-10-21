96d3ee0228d0ba206fd62fb4be1e8de9d5fefb17
kikk <kikpanda@gmail.com>
2019-10-21 14:52:50 +0700

    error handling

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ CORS(app)
 
 @app.route('/getsimscore', methods=['POST']) #GET requests will be blocked
 def json_example():
-    req_data = request.get_json()
-    print(req_data)
-    data = req_data['filelist']
-    result = getsimscore(data)
-    print("DONE")
-    print(result)
-    return jsonify(result)
+    try:
+        req_data = request.get_json()
+        print(req_data)
+        data = req_data['filelist']
+        result = getsimscore(data)
+        print("DONE")
+        print(result)
+        return jsonify(result)
+    except Exception as error:
+        print("API",error)
+        return jsonify({'error': "An error occur",'error_msg':str(error)})
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000) #run app in debug mode on port 5000
